aa1c08737935492996d4fc976c5bfd27784a4cc8
Duc Tran <xtran2@uic.edu>
2025-03-18 16:45:51 -0500

    Restyling frontend

--- iWiH_Django/settings.py
+++ iWiH_Django/settings.py
@@ -14,9 +14,17 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
-MEDIA_URL = '/media/'  # URL for serving media files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'myapp/media/'  # URL for serving media files
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+
+STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, images) will be stored in the 'static' directory within each app.
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # This points to the 'static' folder in your project root
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, HttpResponse
-
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
+
+
 def home(request):
     return render(request, "home.html")
 
+'''
 def uploadPdf(request):
     return render(request, "pdfUpload.html")
-
+'''
 def resultHTML(request):
     pass
 
+def uploadPdf(request):
+    context = {}
+    if request.method == 'POST' and request.FILES['pdf_file']:
+        uploaded_file = request.FILES['pdf_file']
+        fs = FileSystemStorage(location='media')  # Save to the media folder
+        name = fs.save(uploaded_file.name, uploaded_file)  # Save the file
+        context['url'] = fs.url(name)  # Get the URL to access the file
+    return render(request, 'pdfUpload.html', context)
