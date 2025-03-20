63512ac55f5c1139b18230758ce9cd2da6d6e724
Duc Tran <xtran2@uic.edu>
2025-03-19 20:44:53 -0500

    Reduce Tensorflow burden attempt 2

--- myapp/classification.py
+++ myapp/classification.py
@@ -10,6 +10,9 @@ from dotenv import load_dotenv
 from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iWiH_Django.settings')
 django.setup()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 tf.config.set_visible_devices([], 'GPU')
 class GenderClassifier:
     def __init__(self):
